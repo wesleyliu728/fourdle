886d5786ab6835d84b7d27de40f81a0063d692e9
weyliu <weyliu@ucsc.edu>
2022-02-27 16:18:46 -0800

    Fixed some small errors. changed letters to be uppercase

--- src/App.js
+++ src/App.js
@@ -19,23 +19,33 @@ class Board extends React.Component{
       curridx: this.props.curridx,
     };
   }
-  genColors(val, idx){
+  genColors(val, idx, row){
     var color = ""
-    if(val == " " || idx == this.props.curridx){
-      color = "white"
+    if(val == " " || row == this.props.curridx){
+      color = "#fff"
     } else if(this.state.word[idx] == val){
-      color = "green"
+      color = "#6aaa64"
     } else if(this.state.word.includes(val)){
-      color = "orange"
+      color = "#c9b458"
     } else{
-      color = "grey"
+      color = "#86888a"
     }
     return(color)
   }
-  renderSquare(val, idx){
+  returnUpperCase(val){
+    if(val != " "){
+      return(
+        val.toUpperCase()
+      )
+    }
+    else{
+      return(val)
+    }
+  }
+  renderSquare(val, idx, row){
     return(
-      <div className = "square" style = {{background:this.genColors(val,idx),}}>
-        {val}
+      <div className = "square" style = {{background:this.genColors(val,idx, row),}}>
+        {this.returnUpperCase(val)}
       </div>
     )
   }
@@ -55,7 +65,7 @@ class Board extends React.Component{
         list2.map((i)=> {return(
           <div className = "container">
             {list.map((n)=> {return(
-                this.renderSquare(this.props.board[i][n], i)
+                this.renderSquare(this.props.board[i][n], n, i)
               )})}
           </div>
         )})
@@ -95,31 +105,38 @@ class Game extends React.Component{
     document.addEventListener("keydown", this.handleKeyDown);
   }
  handleKeyDown = (event)=>{
-        if(event.keyCode == 13 && this.state.currword.length == 4){ //e.keycode==8
-          this.submit();
-        }else if(event.keyCode == 8 && this.state.currword.length > 0){
-          var temp = this.state.board.slice()
-          var tempword = temp[this.state.gameindex]
-          tempword = tempword.slice(0,tempword.length-1)
-          temp[this.state.gameindex] = tempword;
-          this.setState({
-            currword: temp[this.state.gameindex],
-            board: temp
-          })
-        }else if(this.state.currword.length<4 && ((event.keyCode > 64 && event.keyCode < 91) || (event.keyCode > 96 && event.keyCode < 123))){
-          var currword = this.state.currword.slice()
-          currword = currword + event.key
-          for(var x = 0; x<=this.state.currword.length-4;x++){
-            currword = currword + " "
-          }
-          var temp = this.state.board.slice()
-          temp[this.state.gameindex] = currword
-          this.setState({
-            currword: currword,
-            board: temp,
-          })
-        }
-      }
+  if(this.state.gameindex > 3){
+    return
+  }
+  var curr = this.state.currword.replace(/ /g, '')
+  if(event.keyCode == 13 && curr.length == 4){ //e.keycode==8
+    this.submit();
+  }else if(event.keyCode == 8 && curr.length > 0){
+    var temp = this.state.board.slice()
+    var tempword = curr
+    tempword = tempword.slice(0,tempword.length-1)
+    for(var x = 0; x<=4-curr.length;x++){
+      tempword = tempword + " "
+    }
+    temp[this.state.gameindex] = tempword;
+    this.setState({
+      currword: tempword,
+      board: temp
+    })
+  }else if(curr.length<4 && ((event.keyCode > 64 && event.keyCode < 91) || (event.keyCode > 96 && event.keyCode < 123))){
+    var currword = curr.slice()
+    currword = currword + event.key
+    for(var x = 0; x<=4-curr.length;x++){
+      currword = currword + " "
+    }
+    var temp = this.state.board.slice()
+    temp[this.state.gameindex] = currword
+    this.setState({
+      currword: currword,
+      board: temp,
+    })
+  }
+}
       
 
 
